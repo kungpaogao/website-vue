bf70ceb6fc13174b7eef23c3f027e20ec3e1ac24
Andrew <kungpaogao@gmail.com>
2019-08-23 03:12:36 -0400

    Added link to colors from homepage - test deployment

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="home topbar-comp content-center">
-    <div class="container max-w-xs my-auto mx-auto flex flex-col p-5">
+    <div class="container max-w-xs my-auto mx-auto flex flex-col p-5 text-text">
       <h1 class="text-3xl font-extrabold font-head text-accent-500">Hello!</h1>
       <h2 class="text-4xl font-extrabold font-head">I'm Andrew Gao.</h2>
       <h3 class="text-md font-body lowercase">CS & Stat @ Cornell</h3>
       <doing class="mt-10" />
+      <router-link to="colors" class="mt-10 font-body"
+        >go to colors >></router-link
+      >
     </div>
   </div>
 </template>
